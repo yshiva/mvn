7321df40220fddaa64482e3547b8b87a596b7566
yshiva <sivasankareddyy@gmail.com>
2018-12-17 12:24:18 +0530

    mvn first comment

---
+++ mvn/mvnrepository/src/main/java/mvnproject/App.java
@@ -0,0 +1,13 @@
+package mvnproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
